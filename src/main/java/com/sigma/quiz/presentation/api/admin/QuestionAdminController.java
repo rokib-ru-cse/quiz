177190dbc611efb8c75a66e2b762e094cd29ff5d
Rokib Ahmed <rokib.ahmed@bizzntek.com>
2023-06-23 19:29:26 +0600

    add methods in level controller

--- src/main/java/com/sigma/quiz/presentation/api/admin/QuestionAdminController.java
+++ src/main/java/com/sigma/quiz/presentation/api/admin/QuestionAdminController.java
@@ -1,12 +1,37 @@
 package com.sigma.quiz.presentation.api.admin;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sigma.quiz.application.usecase.IQuestionUseCase;
+import com.sigma.quiz.application.usecase.IQuestionUseCase;
+import com.sigma.quiz.domain.ReturnReponse;
+import com.sigma.quiz.domain.entities.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin/question")
 public class QuestionAdminController {
 
+    @Autowired
+    private IQuestionUseCase  questionUseCase;
 
+    @GetMapping
+    public ReturnReponse<Question> getQuestions(){
+        return questionUseCase.getQuestions();
+    }
+
+    @PostMapping
+    public ReturnReponse<Question> saveQuestion(Question subjectRequest){
+        return questionUseCase.saveQuestion(subjectRequest);
+    }
+
+    @PutMapping
+    public ReturnReponse<Question> updateQuestion(Question subjectRequest){
+        return questionUseCase.updateQuestion(subjectRequest);
+    }
+
+    @DeleteMapping
+    public ReturnReponse<Question> deleteQuestion(@RequestParam int id){
+        return questionUseCase.deleteQuestion(id);
+    }
 
 }
--- src/main/java/com/sigma/quiz/presentation/api/client/QuestionClientController.java
+++ src/main/java/com/sigma/quiz/presentation/api/client/QuestionClientController.java
@@ -1,5 +1,10 @@
 package com.sigma.quiz.presentation.api.client;
 
+import com.sigma.quiz.application.usecase.IQuestionUseCase;
+import com.sigma.quiz.domain.ReturnReponse;
+import com.sigma.quiz.domain.entities.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionClientController {
 
 
+    @Autowired
+    private IQuestionUseCase questionUseCase;
 
+    @GetMapping
+    public ReturnReponse<Question> getQuestions(){
+        return questionUseCase.getQuestions();
+    }
 
 }
