309e57d8351eb4c73a42a71e08b1969fd6e7f449
rokib_ru_cse <rokib.ahmed@bizzntek.com>
2023-08-12 22:29:53 +0600

    .

--- src/main/java/com/sigma/quiz/QuizApplication.java
+++ src/main/java/com/sigma/quiz/QuizApplication.java
@@ -2,10 +2,14 @@ package com.sigma.quiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 @SpringBootApplication
 public class QuizApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuizApplication.class, args);
+		 SpringApplication.run(QuizApplication.class, args);
 	}
+
+
 }
